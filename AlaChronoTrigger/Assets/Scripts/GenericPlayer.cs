ed3e059c50cd2b3a73cde571744b9c215d2f74c9
hedgejess <hedgejess@gmail.com>
2021-11-15 06:56:38 -0600

    syntactic error fixes, problem with constructor fixed

--- AlaChronoTrigger/Assets/Scripts/GenericPlayer.cs
+++ AlaChronoTrigger/Assets/Scripts/GenericPlayer.cs
@@ -4,36 +4,45 @@ using UnityEngine;
 
 public class GenericPlayer : MonoBehaviour
 {
-    public class Stats
+    public class Player
     {
         public int attack;
         public int defense;
         public int health;
         public string playerClass;
         public string name;
+
+        //Custom values for att, def, hp for generic character?
+        public Player(int att, int def, int hp)
+        {
+            attack = att;
+            defense = def;
+            health = hp;
+        }
+
+        //Default values for att, def, hp
+        //Custom values for Character name and class
+        public Player(string charName, string classType)
+        {
+            name = charName;
+            playerClass = classType;
+            attack = 10;
+            defense = 10;
+            health = 20;
+
+        }
+
+        public Player()
+        {
+            attack = 10;
+            defense = 10;
+            health = 20;
+        }
+
+
     }
 
-    //Custom values for att, def, hp for generic character?
-    public Stats(int att, int def, int hp)
-    {
-        attack = att;
-        defense = def;
-        health = hp;
-    }
-    
-    //Default values for att, def, hp
-    //Custom values for Character name and class
-    public Stats(string charName, string classType)
-    {
-        name = charName;
-        playerClass = classType;
-        attack = 10;
-        defense = 10;
-        health = 20;
-
-    }
-
-    public Stats playerStats = new Stats(10, 10, 20);
+    public Player playerStats = new Player(10, 10, 20);
 
     // Start is called before the first frame update
     void Start()
